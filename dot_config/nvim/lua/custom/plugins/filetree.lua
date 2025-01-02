c21633815eded13516328b0a955a7c12815411db
Matteo Bortolazzo <matteobortolazzo@pm.me>
2025-01-02 14:35:14 +0100

    Add nvim configuration

---
+++ dot_config/nvim/lua/custom/plugins/filetree.lua
@@ -0,0 +1,15 @@
+-- File: lua/custom/plugins/filetree.lua
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {}
+  end,
+  vim.keymap.set('n', '<leader>pt', ':Neotree<CR>', { desc = '[P]anel Neo[T]ree' }),
+}
